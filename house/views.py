4c522ed16617e6417deab2793c8a3f69ba884500
sdk2611 <sdk2611@gmail.com>
2021-05-06 01:25:51 +0300

    Modified templates to display posts from database.

--- house/views.py
+++ house/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import house
+from django.utils import timezone
 
 # Create your views here.
 def house_list(request):
-    return render(request, 'house/house_list.html', {})
+    houses = house.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'house/house_list.html', {'houses' : houses})
