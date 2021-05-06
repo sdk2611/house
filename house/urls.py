0682970f60c8df1c93b4f606237a660bce13d1b3
sdk2611 <sdk2611@gmail.com>
2021-05-06 13:00:39 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- house/urls.py
+++ house/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.house_list, name='house_list'),
+    path('house/<int:pk>/', views.house_detail, name='house_detail'),
 ]
--- house/views.py
+++ house/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import house
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def house_list(request):
     houses = house.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'house/house_list.html', {'houses' : houses})
+
+def house_detail(request, pk):
+    _house = get_object_or_404(house, pk=pk)
+    return render(request, 'house/house_detail.html', {'house' : _house})
