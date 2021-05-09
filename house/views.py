1eb5364e2886f069dc50fcb06b90eb7d35afa5b8
sdk2611 <sdk2611@gmail.com>
2021-05-09 15:59:35 +0300

    Логин в систему

--- house/views.py
+++ house/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from .models import house
 from .forms import HouseForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here. 
 def house_list(request):
@@ -11,12 +12,12 @@ def house_list(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'house/house_list.html', {'page_obj': page_obj})
-#    return render(request, 'house/house_list.html', {'houses' : houses})
 
 def house_detail(request, pk):
     _house = get_object_or_404(house, pk=pk)
     return render(request, 'house/house_detail.html', {'house' : _house})
 
+@login_required
 def house_new(request):
     if request.method == "POST":
         form = HouseForm(request.POST)
@@ -30,6 +31,7 @@ def house_new(request):
         form = HouseForm()
     return render(request, 'house/house_edit.html', {'form': form})
 
+@login_required
 def house_edit(request, pk):
     _house = get_object_or_404(house, pk=pk)
     if request.method == "POST":
