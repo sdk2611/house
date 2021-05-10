0fa8720def19da8c660c6de022e81d4c7a5abfd9
sdk2611 <sdk2611@gmail.com>
2021-05-10 17:37:40 +0300

    Upgrade Django, install import-export

--- house/admin.py
+++ house/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import house
-from .models import organization
+from .models import *
 
-admin.site.register(house)
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportMixin
+
+class HouseResource(resources.ModelResource):
+    class Meta:
+        model = house
+
+class HouseAdmin(ImportExportModelAdmin):
+    resource_class = HouseResource
+    pass
+
 admin.site.register(organization)
+admin.site.register(house, HouseAdmin)
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'house',
+    'import_export',
 ]
 
 MIDDLEWARE = [
